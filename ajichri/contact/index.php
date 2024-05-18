e81cd2c76da6177b9c2a216977de5931686b216a
Hamza Sabouri <143541920+hamzasabouri@users.noreply.github.com>
2024-05-19 00:33:33 +0100

    Add files via upload

---
+++ ajichri/contact/index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONTACTS US</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="logo.png">
+
+</head>
+<body id="top">
+
+  <!-- 
+    - #HEADER
+  -->
+
+  <header class="header" data-header>
+    <div class="container">
+
+      <div class="overlay" data-overlay></div>
+
+      <a href="../index/index.php" class="logo">
+        <img src="logo.png" width="160" height="50" alt="ajichri logo">
+      </a>
+
+      <button class="nav-open-btn" data-nav-open-btn aria-label="Open Menu">
+        <ion-icon name="menu-outline"></ion-icon>
+      </button>
+
+      <nav class="navbar" data-navbar>
+
+        <button class="nav-close-btn" data-nav-close-btn aria-label="Close Menu">
+          <ion-icon name="close-outline"></ion-icon>
+        </button>
+
+       
+        <a href="../index/index.php" class="logo">
+          <img src="./assets/images/logo.svg" width="190" height="50" alt="Footcap logo">
+        </a>
+
+        <ul class="navbar-list">
+
+          <li class="navbar-item">
+            <a href="../index/index.php" class="navbar-link">Home</a>
+          </li>
+          <li class="navbar-item">
+            <a href="../produits/index.php" class="navbar-link">Products</a>
+          </li>
+          <li class="navbar-item">
+            <a href="../categorie/index.php" class="navbar-link">Categorie</a>
+          </li>
+          <li class="navbar-item">
+            <a href="../search/index.php" class="navbar-link">Search</a>
+          </li>
+
+          <li class="navbar-item">
+            <a href="../contact/index.php" class="navbar-link">Contact</a>
+          </li>
+
+        </ul>
+
+        <ul class="nav-action-list">
+
+          <li>
+          <div class="input-container">
+          <a href="../search/index.php" class="nav-action-btn">
+<i class="fa-solid fa-magnifying-glass fa-bounce"></i>          </li>
+</a>
+          <li>
+            <a href="../login/login.php" class="nav-action-btn">
+              <ion-icon name="person-outline" aria-hidden="true" href="./login/login.php"></ion-icon>
+
+              <span class="nav-action-text">Login / Register</span>
+            </a>
+          </li>
+
+
+
+          <li>
+          <?php
+$conn = mysqli_connect('localhost:3307', 'root', '', 'ajichri');
+
+      
+      $select_rows = mysqli_query($conn, "SELECT * FROM `cart`") or die('query failed');
+      $row_count = mysqli_num_rows($select_rows);
+
+      ?>
+
+            <button class="nav-action-btn">
+            <a class="nav-action-btn" href="../panier/index.php">
+              <ion-icon name="bag-outline" aria-hidden="true" href="../panier/index.php"></ion-icon><span><?php echo $row_count; ?></span> </a>
+              </a>
+</div>
+          </li>
+
+        </ul>
+
+      </nav>
+
+    </div>
+  </header>
+
+  </div>
+  </div>
+  </div>
+
+  <div class="background" >
+  <div class="container">
+    <div class="screen">
+      <div class="screen-header">
+        <div class="screen-header-left">
+          <div class="screen-header-button close"></div>
+          <div class="screen-header-button maximize"></div>
+          <div class="screen-header-button minimize"></div>
+        </div>
+        <div class="screen-header-right">
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+        </div>
+      </div>
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <div class="app-title">
+            <span>CONTACT</span>
+            <span>US</span>
+          </div>
+          <div class="app-contact">CONTACT INFO : +212 7628462</div>
+        </div>
+        <div class="screen-body-item">
+          <div class="app-form" >
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME" >
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL">
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="CONTACT NO">
+            </div>
+            <div class="app-form-group message">
+              <input class="app-form-control" placeholder="MESSAGE">
+            </div>
+            <div class="app-form-group buttons">
+              <button class="app-form-button">CANCEL</button>
+              <button class="app-form-button">SEND</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+  <!-- 
+    - #GO TO TOP
+  -->
+
+  <a href="#top" class="go-top-btn" data-go-top>
+    <ion-icon name="arrow-up-outline"></ion-icon>
+  </a>
+
+
+
+
+
+  <!-- 
+    - custom js link
+  -->
+  <script src="script.js"></script>
+
+  <!-- 
+    - ionicon link
+  -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script><script src="https://kit.fontawesome.com/e51b85f8d2.js" crossorigin="anonymous"></script>
+</body>
+</html>
